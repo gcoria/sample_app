c6791b0feb81c47e01e65ef025cd9249c276e41b
Gaston Coria <marianopolitano@hotmail.com>
2013-09-16 21:56:45 -0300

    Finish user pages

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
   gem 'guard-rspec', '1.2.1'
+  gem 'factory_girl_rails', '4.1.0'
+
 end
 
 group :development do
@@ -52,6 +54,8 @@ group :test do
   gem 'capybara', '1.1.2'
   gem 'rb-inotify', '0.8.8'
   gem 'libnotify', '0.5.9'
+  gem 'factory_girl_rails', '4.1.0'
+
 end
 
 group :production do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -91,6 +91,57 @@ describe User do
 
     it { should_not be_valid }
   end
+   
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
+  describe "User pages" do
+
+    subject { page }
+
+    describe "signup" do
+
+      before { visit signup_path }
+
+      let(:submit) { "Create my account" }
+
+      describe "with invalid information" do
+        it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+        end
+      end
+
+      describe "with valid information" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+      end
+
+        it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+
+
+  end  
     
+  describe "profile page" do
+    # Code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
 end
 
